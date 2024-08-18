13c678e8f3d881303cdac964487023c2b391dd27
Goolo2 <1243566178@qq.com>
2024-08-18 19:06:40 +0800

    update

--- estimate_3.py
+++ estimate_3.py
@@ -36,34 +36,24 @@ class sos_objective(objective_function):
         self.default_blur=1.0
 
     def evaluate_function(self, params=None, xs=None, ys=None, ts=None,
-            warpfunc=None, img_size=None, blur_sigma=None, showimg=False, iwe=None):
+            warpfunc=None, img_size=None, blur_sigma=None, showimg=False, iwe=None, device='cpu'):
         """
         Loss given by g(x)^2 where g(x) is IWE
         """
         if iwe is None:
-            iwe = get_iwe(params, xs, ys, ts, warpfunc, img_size, use_polarity=self.use_polarity, compute_gradient=False)
+            iwe = get_iwe(params, xs, ys, ts, warpfunc, img_size, use_polarity=self.use_polarity, compute_gradient=False, device=device)
         blur_sigma=self.default_blur if blur_sigma is None else blur_sigma
-        # return np.random.rand()
 
-        # sos = np.mean(iwe*iwe)#/num_pix
-        sos = torch.mean(iwe*iwe)#/num_pix
+        sos = torch.mean(iwe*iwe)
         return -sos
-        
-        # loss = np.var(iwe-np.mean(iwe))
-        # return -loss
-        
-        # exp = np.exp(iwe.astype(np.double))
-        # soe = np.mean(exp)
-        # return -soe
 
-def events_bounds_mask(xs, ys, x_min, x_max, y_min, y_max):
+
+def events_bounds_mask(xs, ys, x_min, x_max, y_min, y_max, device='cpu'):
     """
     Get a mask of the events that are within the given bounds
     """
-    # mask = np.where(np.logical_or(xs<=x_min, xs>x_max), 0.0, 1.0)
-    # mask *= np.where(np.logical_or(ys<=y_min, ys>y_max), 0.0, 1.0)
-    mask = torch.where(torch.logical_or(xs<=x_min, xs>x_max), 0.0, 1.0)
-    mask *= torch.where(torch.logical_or(ys<=y_min, ys>y_max), 0.0, 1.0)
+    mask = torch.where(torch.logical_or(xs<=x_min, xs>x_max), torch.tensor(0.0).to(device), torch.tensor(1.0).to(device))
+    mask *= torch.where(torch.logical_or(ys<=y_min, ys>y_max), torch.tensor(0.0).to(device), torch.tensor(1.0).to(device))
     return mask
 
 
@@ -92,9 +82,9 @@ def interpolate_to_derivative_img(pxs, pys, dxs, dys, d_img, w1, w2):
 def events_to_image_drv(xn, yn, pn, jacobian_xn, jacobian_yn,
         device=None, sensor_size=(180, 240), clip_out_of_range=True,
         interpolation=None, padding=True, compute_gradient=False):
-    # xt, yt, pt = torch.from_numpy(xn), torch.from_numpy(yn), torch.from_numpy(pn)
     xt, yt, pt = xn, yn, pn
     xs, ys, ps, = xt.float(), yt.float(), pt.float()
+
     if device is None:
         device = xs.device
     if padding:
@@ -102,10 +92,10 @@ def events_to_image_drv(xn, yn, pn, jacobian_xn, jacobian_yn,
     else:
         img_size = sensor_size
 
-    mask = torch.ones(xs.size())
+    mask = torch.ones(xs.size()).to(device)
     if clip_out_of_range:
-        zero_v = torch.tensor([0.])
-        ones_v = torch.tensor([1.])
+        zero_v = torch.tensor([0.]).to(device)
+        ones_v = torch.tensor([1.]).to(device)
         clipx = img_size[1] if interpolation is None and padding==False else img_size[1]-1
         clipy = img_size[0] if interpolation is None and padding==False else img_size[0]-1
         mask = torch.where(xs>=clipx, zero_v, ones_v)*torch.where(ys>=clipy, zero_v, ones_v)
@@ -113,32 +103,24 @@ def events_to_image_drv(xn, yn, pn, jacobian_xn, jacobian_yn,
     pxs = xs.floor()
     pys = ys.floor()
     dxs = xs-pxs
-    dys = ys-pys
+    dys = ys-pxs
     pxs = (pxs*mask).long()
     pys = (pys*mask).long()
     masked_ps = ps*mask
     img = torch.zeros(img_size).to(device)
     interpolate_to_image(pxs, pys, dxs, dys, masked_ps, img)
 
-
-    # return img.numpy()
     return img
 
 
-def get_iwe(params, xs, ys, ts, warpfunc, img_size, compute_gradient=False, use_polarity=True):
-    """
-    Given a set of parameters, events and warp function, get the warped image and derivative image
-    if required.
-    """
-    xs, ys, jx, jy = warpfunc.warp(xs, ys, ts, ts[-1], params)
-    mask = events_bounds_mask(xs, ys, 0, img_size[1], 0, img_size[0])
+def get_iwe(params, xs, ys, ts, warpfunc, img_size, compute_gradient=False, use_polarity=True, device='cpu'):
+    xs, ys, jx, jy = warpfunc.warp(xs, ys, ts, ts[-1], params, device=device)
+    mask = events_bounds_mask(xs, ys, 0, img_size[1], 0, img_size[0], device=device)
     xs, ys = xs*mask, ys*mask
     
-    # ps = np.ones_like(xs)
-    ps = torch.ones_like(xs)
-
+    ps = torch.ones_like(xs).to(device)
     iwe = events_to_image_drv(xs, ys, ps, jx, jy, sensor_size=img_size,
-            interpolation='bilinear', compute_gradient=compute_gradient)
+            interpolation='bilinear', compute_gradient=compute_gradient, device=device)
     return iwe
 
 
@@ -154,22 +136,18 @@ class rotvel_warp(warp_function):
     """
     This class implements rotational velocity warping
     """
-    def __init__(self,centers=None):
+    def __init__(self, centers=None):
         warp_function.__init__(self, 'rotvel_warp', 1)
         self.centers = centers
 
-    def warp(self, xs, ys, ts, t0, params):
-        xs_copy = torch.from_numpy(xs)
-        ys_copy = torch.from_numpy(ys)
-        ts_copy = torch.from_numpy(ts)
-        t0 = torch.tensor(t0)
+    def warp(self, xs, ys, ts, t0, params, device='cpu'):
+        xs_copy = torch.from_numpy(xs).to(device)
+        ys_copy = torch.from_numpy(ys).to(device)
+        ts_copy = torch.from_numpy(ts).to(device)
+        t0 = torch.tensor(t0).to(device)
         dt = ts_copy-t0
         theta = dt*params[0]
 
-        # xs_copy = xs.copy()
-        # ys_copy = ys.copy()
-        # ts_copy = ts.copy()
-        
         if self.centers:
             xs_mean = self.centers[0]
             ys_mean = self.centers[1]
@@ -180,7 +158,6 @@ class rotvel_warp(warp_function):
         xs_copy = xs_copy - xs_mean
         ys_copy = ys_copy - ys_mean
         
-        # rotation matrix
         cos_theta = torch.cos(theta)
         sin_theta = torch.sin(theta)
 
@@ -193,47 +170,27 @@ class rotvel_warp(warp_function):
         jacobian_x, jacobian_y = None, None
 
         return x_prime, y_prime, jacobian_x, jacobian_y
-    
-    
-    
-# def optimize(xs, ys, ts, warp_function, objective, optimizer=opt.fmin_bfgs, x0=None, blur_sigma=None, img_size=(180, 240)):
-#     args = (xs, ys, ts, warp_function, img_size, blur_sigma)
-#     x0 = np.array([-np.deg2rad(12000)])
-#     # bounds = (np.array([-np.deg2rad(20000)]), np.array([-np.deg2rad(-20000)]))
-#     bounds = (np.array([-np.deg2rad(20000)]), np.array([-np.deg2rad(7000)]))
-#     print(bounds, x0)
-#     if x0 is None:
-#         x0 = np.zeros(warp_function.dims)
-#     argmax = opt.minimize_scalar(objective.evaluate_function, args=args, bounds=bounds, method='bounded')
-#     print(argmax.x)
-#     return argmax.x
-    
-def optimize(xs, ys, ts, warp_function, objective, x0=None, blur_sigma=None, img_size=(180, 240), lr=0.01, num_epochs=100):
-    # x0 = torch.tensor(x0, requires_grad=True)
-    x0 = torch.tensor([-np.deg2rad(12000)], requires_grad=True)
-    # 选择优化器
-    optimizer = torch.optim.SGD([x0], lr=1)
+
+
+def optimize(xs, ys, ts, warp_function, objective, x0=None, blur_sigma=None, img_size=(180, 240), lr=0.01, num_epochs=100, device='cpu'):
+    x0 = torch.tensor([-np.deg2rad(12000)], requires_grad=True, device=device)
+
+    optimizer = torch.optim.SGD([x0], lr=lr)
     
     for epoch in range(num_epochs):
         optimizer.zero_grad()
         
-        # 计算目标函数值
-        loss_value = objective.evaluate_function(params=x0, xs=xs, ys=ys, ts=ts, warpfunc=warp_function, img_size=img_size, blur_sigma=blur_sigma)
+        loss_value = objective.evaluate_function(params=x0, xs=xs, ys=ys, ts=ts, warpfunc=warp_function, img_size=img_size, blur_sigma=blur_sigma, device=device)
         loss = loss_value
-        
-        # 计算梯度
+
         loss.backward()
         
-        # 执行优化步骤
         optimizer.step()
-        
-        # grads = x0.grad
-        # print(f'Gradient: {grads}')
         
         if (epoch + 1) % 10 == 0:
             print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss_value.item()}')
     
-    return x0.detach().numpy()
+    return x0.detach().cpu().numpy()
 
 
 def get_data(data):
@@ -248,44 +205,24 @@ def get_data(data):
     return xs, ys, ts
 
 
-
-def calculate_rpm(data, img_size, obj, blur, centers):
+def calculate_rpm(data, img_size, obj, blur, centers, device='cpu'):
     xs, ys, ts = get_data(data)
 
-    warp = rotvel_warp(centers) #!############### very important #############################
-    # 
-    argmax = optimize(xs, ys, ts, warp, obj, img_size=img_size, blur_sigma=blur)
+    warp = rotvel_warp(centers)
+    argmax = optimize(xs, ys, ts, warp, obj, img_size=img_size, blur_sigma=blur, device=device)
     
     rpm = np.rad2deg(argmax)/360*60
 
     return rpm.tolist()[0], argmax
 
 
-
-
-def custom_optimize(data):
+def custom_optimize(data, img_size, obj, device='cpu'):
     xs, ys, ts = get_data(data)
-    # ?choose objective function#########################################
-    # obj = variance_objective()
-    # obj = rms_objective()
-    obj = sos_objective()
-    # obj = soe_objective()
-    # obj = moa_objective()
-    # obj = isoa_objective()
-    # obj = sosa_objective()
-    # obj = tsmap_objective()
-    # obj = timestamp_objective()
-    # obj = biobj_objective()
-
-    warp = rotvel_warp([600,411])
-    losses= []
-    for i in range(-50,50):
+    warp = rotvel_warp([600, 411])
+    losses = []
+    for i in range(-50, 50):
         params = np.array([-np.deg2rad(i*1000)])
-        objval = obj.evaluate_function(params, xs, ys, ts, warp, img_size)
-        # iwe, d_iwe = get_iwe(params, xs, ys, ts, warp, img_size, use_polarity=False, compute_gradient=False)
-        # print(iwe.shape)
-        # objval = -np.var(iwe-np.mean(iwe))
-        
+        objval = obj.evaluate_function(params, xs, ys, ts, warp, img_size, device=device)
         losses.append((i, params[0], objval))
         print(i, params[0], objval)
     
@@ -295,55 +232,39 @@ def custom_optimize(data):
     print(min_loss_i, rad, min_loss_value, rpm)
     
     losses = np.array(losses)
-    plt.scatter(losses[:,0], losses[:,2], color='g', s=5)
+    plt.scatter(losses[:, 0], losses[:, 2], color='g', s=5)
     plt.show()
-    
-    
+
+
 if __name__ == "__main__":
-    """
-    Quick demo of various objectives.
-    Args:
-        path Path to h5 file with event data
-        gt Ground truth optic flow for event slice
-        img_size The size of the event camera sensor
-    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--gt", nargs='+', type=float, default=(0, 0))
-    parser.add_argument("--img_size", nargs='+',
-                        type=float, default=(720, 1280))  # x,y
-    # parser.add_argument("--img_size", nargs='+', type=float, default=(-100,100))
+    parser.add_argument("--img_size", nargs='+', type=float, default=(720, 1280))  # x,y
+    parser.add_argument("--device", type=str, default="cpu", choices=["cpu", "cuda"])  # Device choice
     args = parser.parse_args()
 
     img_size = tuple(args.img_size)
-
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    print(device)
     obj = sos_objective()
 
-    '''calculate rpm for multiple blades'''
-
-    for keyname in range(1,2,1):        
-        # basedir = f'H:\propeller_det_data\\testbed_exp\data\event\\processed\diff_illumination/{keyname}'
+    for keyname in range(1, 2, 1):
         basedir = f'./'
-        # savedir = f'./results/ours/diff_illumination/{keyname}_filterd_{obj.name}_latency.csv'
         savedir = f'test.csv'
         
         print(f'Processing {keyname}...')
 
-        
-        # blur = 2.0 
-        blur = 0.0 
-        centers = [600,411]
+        blur = 0.0
+        centers = [600, 411]
         
         rpms_res = []
         filepath = os.path.join(basedir, f'cluster_1.npy')
         alldata = np.load(filepath, allow_pickle=True)
         for idx, data in enumerate(alldata):
-            # data = data[:1000]
             begin = time.time()
-            # custom_optimize(data)
-            rpm, rad_s = calculate_rpm(data, img_size, obj, blur, centers)
+            rpm, rad_s = calculate_rpm(data, img_size, obj, blur, centers, device=device)
             end = time.time()
             
-            # print(idx/len(alldata)*100.0, rad_s)
             print(f' {idx}/{len(alldata)}:  rpm = {abs(rpm)}')
 
             timestamp = idx * 50e3
@@ -351,8 +272,5 @@ if __name__ == "__main__":
             print(f'Latency: {delta_t:.6f}s')
             rpms_res.append([timestamp, abs(rpm), delta_t])
 
-        
         df_rpms = pd.DataFrame(rpms_res, columns=['Time', 'RPM', 'DeltaT'])
         df_rpms.to_csv(savedir, index=False)
-        
-
